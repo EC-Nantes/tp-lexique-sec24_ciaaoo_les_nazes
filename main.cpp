9d539c341555718644c794fce26f7401e5beffa0
yadon95 <yanis.keto@eleves.ec-nantes.fr>
2022-11-25 09:43:40 +0100

    compile

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <Windows.h>
+
 #include "lexique.h"
 
 int main(int argv, const char * args[]){
+    SetConsoleOutputCP(CP_UTF8);
+
     Lexique lexique;
 
     lexique.read_file(args[1]);
