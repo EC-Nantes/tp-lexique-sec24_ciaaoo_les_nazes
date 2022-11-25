e951cd7160831126565a189fbe58f306c75610e4
yadon95 <yanis.keto@eleves.ec-nantes.fr>
2022-11-25 11:19:49 +0100

    lexique_lignes

---
+++ lexique_ligne.h
@@ -0,0 +1,11 @@
+#include "lexique.h"
+#include <vector>
+
+class Lexique_lignes : Lexique {
+    protected :
+        map<string, vector<int>> lexique_lignes;
+
+    public :
+        Lexique_lignes();
+        
+};
