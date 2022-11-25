51efbbad778b79b862c7cf1bbd7a63ae789a0b82
yadon95 <yanis.keto@eleves.ec-nantes.fr>
2022-11-25 09:08:42 +0100

    read file function

--- lexique.cpp
+++ lexique.cpp
@@ -1,5 +1,27 @@
 #include "lexique.h"
 
 int lexique :: read_file(string path){
-    if()
+
+    string content; // file's content
+
+    readFileIntoString(path, content);  // reading the file in content
+    
+    // Was the file empty ?
+    if(content.size() == 0){
+        return 1;   // file empty
+    }
+
+    string word;
+
+    // Reading the file, spliting it word by word, and adding it to the lexique
+    for(int i = 0; i < content.size(); i++){
+        if(content[i] == ' ' || content[i] == '\n'){
+            addWord(word);
+            word = "";
+        } else {
+            word += content[i];
+        }
+    }
+
+    return 0;
 }
