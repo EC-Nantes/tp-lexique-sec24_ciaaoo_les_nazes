053750bdacd979e42bf3f411add8b7c9bce937e6
yadon95 <yanis.keto@eleves.ec-nantes.fr>
2022-11-25 11:50:38 +0100

    lines

--- lexique_ligne.h
+++ lexique_ligne.h
@@ -1,11 +1,13 @@
 #include "lexique.h"
 #include <vector>
 
-class Lexique_lignes : Lexique {
+class Lexique_lignes : public Lexique {
     protected :
         map<string, vector<int>> lexique_lignes;
 
     public :
         Lexique_lignes();
-        
+        int addWord(string word, int line);
+        int removeWord(string word);
+        void printLexique();
 };
