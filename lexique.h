9fb087d976edec9de4608111fb1b3992dd9679da
Clément Jouin <95017825+ClementEcNantes@users.noreply.github.com>
2022-11-25 08:50:10 +0100

    class lexique

--- lexique.h
+++ lexique.h
@@ -1,11 +1,12 @@
 #define lexique_h
-
+#include <stdio.h>
 #include <iostream>
+using namespace std;
+#include <map>
 
 class lexique{
 protected:
-    char nom; 
-    int occurence;
+    map<string, int> words;
 
 public:
     lexique(char nom);
